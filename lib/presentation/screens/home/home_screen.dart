c96c92b92d7cabfa29e489738b839e4ac5ab12c0
MadLain-L <joncabdev@gmail.com>
2025-08-29 15:16:03 -0600

    implementacion de go_router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -49,7 +49,8 @@ class _CustomListTitle extends StatelessWidget {
         //Navigator.of(context).push(
         //  MaterialPageRoute<void>(builder: (context) => const ButtonsScreen()),
         //);
-        Navigator.pushNamed(context, menuItem.link);
+        //Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
