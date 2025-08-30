6a96d2620576088f71e49c41f1cc7537b49bfc64
MadLain-L <joncabdev@gmail.com>
2025-08-30 09:28:17 -0600

    Se realiza la finalizacion del apartado de botones

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
@@ -7,7 +8,104 @@ class ButtonsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Buttons Screen')),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_back_ios_sharp),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Elevated Button'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: const Text('Elevated Disabled'),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.alarm_add_rounded),
+              label: const Text('Elevated Icon'),
+            ),
+
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            FilledButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.accessibility_new_rounded),
+              label: const Text('Filled icon'),
+            ),
+
+            OutlinedButton(onPressed: () {}, child: const Text('Outlinded')),
+
+            OutlinedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.terminal),
+              label: const Text('Outlined Icon'),
+            ),
+
+            TextButton(onPressed: () {}, child: const Text('TextButton')),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.android),
+              label: const Text('TextButtonIcon'),
+            ),
+
+            const CustomeButton(),
+
+            IconButton(onPressed: () {}, icon: Icon(Icons.cake_outlined)),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.do_not_disturb),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(colors.primary),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomeButton extends StatelessWidget {
+  const CustomeButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Que onda', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ),
     );
   }
 }
