e55f38ea7ac072e7e02664430028636037bd7eae
MadLain-L <joncabdev@gmail.com>
2025-08-30 13:58:43 -0600

    Creacion de tarjetas con bordes ajustables

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -39,7 +39,7 @@ class _CardsView extends StatelessWidget {
             ),
 
             ...cards.map(
-              (card) => _CardType1(
+              (card) => _CardType2(
                 label: card['label'],
                 elevation: card['elevation'],
               ),
@@ -60,6 +60,41 @@ class _CardType1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.accessible_outlined),
+              ),
+            ),
+            Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: colors.outline),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
       elevation: elevation,
       child: Padding(
         padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
