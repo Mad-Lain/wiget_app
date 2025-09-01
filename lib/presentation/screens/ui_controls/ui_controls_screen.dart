dfe633a13a2a82ad066820569407f22cf0d08ba0
MadLain <joncabdev@gmail.com>
2025-09-01 13:07:01 -0600

    Checkbox deprecapted se a hecho la pregunta dentro del foro, en busca de soluciones.

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -20,8 +20,11 @@ class _UiControlsView extends StatefulWidget {
   State<_UiControlsView> createState() => _UiControlsViewState();
 }
 
+enum Transportation {car, plane,boat, submarine }
+
 class _UiControlsViewState extends State<_UiControlsView> {
   bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +40,36 @@ class _UiControlsViewState extends State<_UiControlsView> {
           }),
         ),
 
-        RadioGroup(onChanged: onChanged, child: child),
+        RadioGroup(
+          onChanged: (value) => setState(() {
+          selectedTransportation = Transportation.car;
+        }), child: Column(children: [
+          Radio(value: Transportation.car),
+        ],)),
+
+        RadioGroup(onChanged: (value) => setState(() {
+          selectedTransportation = Transportation.plane;
+        }), child: Column(children: [
+          Radio(value: Transportation.plane)
+        ],)),
+
+        RadioGroup(onChanged: (value) => setState(() {
+          selectedTransportation = Transportation.boat;
+        }), child: Column(children: [
+          Radio(value: Transportation.boat)
+        ],)),
+
+        RadioGroup(onChanged: (value) => setState(() {
+          selectedTransportation = Transportation.submarine;
+        }), child: Column(children: [
+          Radio(value: Transportation.submarine)
+        ],)),
+
+        
+
+        
+
+        
       ],
     );
   }
